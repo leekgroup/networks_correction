5d337b698fc643cee6ec90dbd2da1e2dc2f189ca
Princy Parsana <princyp89@gmail.com>
2018-05-02 09:01:57 -0400

    removing files deleted from fs

--- publication_figures/fig2/fig2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#SBATCH --time=01:00:0
-#SBATCH --mem=30G
-
-cd $parsana/networks_correction/publication_figures/fig2/
-Rscript process_fig2.R
--- src/shellscripts/run_process.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#SBATCH --time=12:0:0
-#SBATCH --mem=70G
-
-source /work-zfs/abattle4/parsana/networks_correction/src/shellscripts/dirconfig
-cd $netcorrectsrc
-
-Rscript process_gtex.R >log/process.log
